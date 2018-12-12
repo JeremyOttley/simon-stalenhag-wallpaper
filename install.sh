46ea5d62150508c10e010f00d924b088ed17146d
Jeremy Ottley <Gazaunga@gmail.com>
2018-12-12 13:38:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo cp stalenhag.py /usr/local/bin/stalenhag
+sudo cp stalenhag.rb /usr/local/bin/stalenhag
 sudo cp -p systemd/* /etc/systemd/user
 systemctl enable --user stalenhag.timer
 systemctl start --user stalenhag.timer
