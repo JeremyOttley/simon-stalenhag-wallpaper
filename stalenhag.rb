20b3e944224897fa9ac1044d5bf803cf957dfcf7
Jeremy Ottley <Gazaunga@gmail.com>
2018-12-13 06:13:13 -0500

    Update stalenhag.rb

--- stalenhag.rb
+++ stalenhag.rb
@@ -1,1 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'pathname'
+require 'net/http'
+require 'uri'
+
+BASE = "http://www.simonstalenhag.se/"
+IMAGE_DIR = _expand_path("~/Pictures/Stålenhag/")
+
+def check_dirs()
+.mkdir_p(IMAGE_DIR) unless Dir.exist?(IMAGE_DIR)
+end
+
+def local_exists(filename)
+File.exist?(IMAGE_DIR/filename)
+end
+
+def get_image_list()
+uri = URI.parse(BASE)
+response = Net::HTTP.get_response(uri)
+contents = response.body
+images = \/[a-zA-Z0-9]*\.jpg
+end
+
+def download_image(name)
+uri.get(BASE + name)
+end
+
+def get_random_local_image
+images = Dir.ls(IMAGES_DIR)
+imagese = 
+images = .extname('.jpg')
+end
 
